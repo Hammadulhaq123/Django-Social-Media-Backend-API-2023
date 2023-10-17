34f2f257ea3c840270b8947f39e18e2c616ab8d7
Hammadulhaq123 <professorcoding123@gmail.com>
2023-10-17 23:09:14 +0500

    Creating views using viewsets from rest_framework to reduce redundancy

--- socialmediabackend/users/views.py
+++ socialmediabackend/users/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
-
+from rest_framework import viewsets
+from rest_framework.response import Response
+from django.contrib.auth.models import User
+from .serializers import UserSerializer
 # Create your views here.
+
+
+class UserViewset(viewsets.Viewset):
+
+    # List
+    def list(self, request):
+        queryset = User.objects.all()
+        serializer = UserSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    # Create
+    def create(self, request):
+        pass
+
+    # Retrieve
+    def retrieve(self, request):
+        pass
