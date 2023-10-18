833a77d9cf9280b714bef73fe3bbb486e5f58235
Hammadulhaq123 <professorcoding123@gmail.com>
2023-10-18 16:19:15 +0500

    Update: Media url updated, Add: Added user_profile app to the settings

--- socialmediabackend/socialmediabackend/settings.py
+++ socialmediabackend/socialmediabackend/settings.py
@@ -48,7 +48,8 @@ EXTENDED_APPS = [
     'users.apps.UsersConfig',
     'feed.apps.FeedConfig',
     'rest_framework',
-    'api'
+    'api',
+    'user_profile'
 ]
 
 INSTALLED_APPS += EXTENDED_APPS
@@ -129,10 +130,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 # Default primary key field type
