b99d84d08511c42af6675b2151531b539e4a38b9
Hammadulhaq123 <professorcoding123@gmail.com>
2023-10-18 20:27:53 +0500

    Update: Added a new url that allows user to access profile details

--- socialmediabackend/api/urls.py
+++ socialmediabackend/api/urls.py
@@ -1,7 +1,9 @@
 from rest_framework.routers import DefaultRouter
 from users.views import UserViewset
+from user_profile.views import UserProfileView
 
 
 router = DefaultRouter()
 router.register(r'users', UserViewset, basename='users')
+router.register(r'profiles', UserProfileView, basename='profiles')
 urlpatterns = router.urls
