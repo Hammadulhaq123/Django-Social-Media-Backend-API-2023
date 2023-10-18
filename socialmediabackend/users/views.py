9981b351ffb04c53412f8884f5d2c7acd47caae2
Hammadulhaq123 <professorcoding123@gmail.com>
2023-10-18 15:37:10 +0500

    Fix: Fixed typo mistakes and wrong importations

--- socialmediabackend/users/views.py
+++ socialmediabackend/users/views.py
@@ -1,12 +1,12 @@
-from rest_framework import viewsets
-from rest_framework.response import Response, status
+from rest_framework import viewsets, status
+from rest_framework.response import Response
 from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404
 from .serializers import UserSerializer
 # Create your views here.
 
 
-class UserViewset(viewsets.Viewset):
+class UserViewset(viewsets.ViewSet):
 
     # List
     def list(self, request):
