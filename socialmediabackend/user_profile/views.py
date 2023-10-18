df1c5a0bd84fcaac4ebcc8c860b56ff0b996f567
Hammadulhaq123 <professorcoding123@gmail.com>
2023-10-18 20:26:00 +0500

    Create: Created a UserProfileView that allows the user to do CRUD Operations on the route

--- socialmediabackend/user_profile/views.py
+++ socialmediabackend/user_profile/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from user_profile.models import UserProfile
+from rest_framework import viewsets
+from rest_framework import permissions
+from . permissions import IsOwnerOrReadOnly
+from . serializers import ProfileSerializer
 
 # Create your views here.
+
+
+class UserProfileView(viewsets.ModelViewSet):
+    queryset = UserProfile.objects.all()
+    serializer_class = ProfileSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly,
+                          IsOwnerOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
