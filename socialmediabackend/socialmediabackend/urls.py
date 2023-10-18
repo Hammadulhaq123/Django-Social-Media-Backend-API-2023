097a458d1067fa6e54b9c3c7b0d28f03683fb72f
Hammadulhaq123 <professorcoding123@gmail.com>
2023-10-18 18:27:03 +0500

    Update: Installed coreapi, pyyaml and uritemplates for generating a schema and docs of the api. Added the paths of the docs,home,and schema view to the main urls

--- socialmediabackend/socialmediabackend/urls.py
+++ socialmediabackend/socialmediabackend/urls.py
@@ -18,11 +18,20 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-
+from rest_framework.schemas import get_schema_view
+from rest_framework.documentation import include_docs_urls
+from api.views import home
 urlpatterns = [
+    path('', home, name="home"),
     path('admin/', admin.site.urls),
+    path('docs/', include_docs_urls(title="Api Documentation")),
     path('api-auth/', include('rest_framework.urls')),
-    path('api/', include('api.urls'))
+    path('api/', include('api.urls')),
+    path('schema/', get_schema_view(
+        title="Social Media Backend",
+        description="An Api build on top of django and drf that serves as a backend for a social media platform",
+        version='1.0.0'
+    ), name="api_schema")
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
