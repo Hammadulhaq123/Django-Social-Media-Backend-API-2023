e617c84b466490736f40e8c60af95c92675e01a6
Hammadulhaq123 <professorcoding123@gmail.com>
2023-10-21 15:12:58 +0500

    Create: Created a serializer to serialize all the comments

--- socialmediabackend/comments/serializers.py
+++ socialmediabackend/comments/serializers.py
@@ -4,4 +4,5 @@ from .models import Comments
 class CommentSerializer(serializers.ModelSerializer):
     commented_by = serializers.ReadOnlyField(source='owner.username')
     class Meta:
+        model = Comments
         fields = ('id', 'commented_by', 'post', 'comment', 'comment_giphy', 'comment_date')
