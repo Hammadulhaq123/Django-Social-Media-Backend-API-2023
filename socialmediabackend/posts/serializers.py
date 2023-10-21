964003ba7936c3fb60d8776126cda2ca91770abb
Hammadulhaq123 <professorcoding123@gmail.com>
2023-10-21 16:12:51 +0500

    Update: Added a new like field in the posts serializer to show all the likes realted to a post with the post

--- socialmediabackend/posts/serializers.py
+++ socialmediabackend/posts/serializers.py
@@ -1,10 +1,12 @@
 from rest_framework import serializers
 from .models import Posts
 from comments.serializers import CommentSerializer
+from likes.serializers import LikesSerializer
 
 
 class PostSerializer(serializers.ModelSerializer):
     comments = CommentSerializer( many=True, read_only=True)
+    likes = LikesSerializer(many=True, read_only=True)
     class Meta:
         model = Posts
-        fields = ('id', 'content', 'post_image', 'comments', 'category', 'post_date')
+        fields = ('id', 'content', 'post_image', 'likes','comments', 'category', 'post_date')
