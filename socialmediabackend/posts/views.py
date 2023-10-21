0f8830fc62cf61dc606a6e08d0fd3d7e18682680
Hammadulhaq123 <professorcoding123@gmail.com>
2023-10-21 13:30:56 +0500

    Fix: Import error fixed

--- socialmediabackend/posts/views.py
+++ socialmediabackend/posts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from rest_framework import permissions, viewsets, status
 from user_profile.permissions import IsOwnerOrReadOnly
+from .serializers import PostSerializer
 from .models import Posts
 
 # Create your views here.
@@ -11,5 +12,6 @@ class PostsViewset(viewsets.ModelViewSet):
     serializer_class = PostSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
 
+    # user can only create a post under his owenrship and can only update or delete his posts not anyone else's
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
