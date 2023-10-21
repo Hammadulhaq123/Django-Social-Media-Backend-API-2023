e8ac85d9c8c4cf8da30486a1832871bae58a02d6
Hammadulhaq123 <professorcoding123@gmail.com>
2023-10-21 21:14:00 +0500

    Update: user can only follow others not himself

--- socialmediabackend/friendships/views.py
+++ socialmediabackend/friendships/views.py
@@ -4,6 +4,7 @@ from rest_framework import serializers, viewsets,status,permissions
 from . models import Friends
 from . serializers import FriendSerializer
 from django.contrib.auth.models import User
+from django.contrib import messages
 
 
 # Create your views here.
@@ -16,10 +17,17 @@ class FriendsViewset(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         user_instance=get_object_or_404(User,pk=self.request.data['user'])
 
-        already_followed=Friends.objects.filter(user=user_instance,friendzoned_by=self.request.user).exists()
-        if already_followed:
-            Friends.objects.filter(user=user_instance,friendzoned_by=self.request.user).delete()
+        if(self.request.user == user_instance):
+            messages.error(self.request,"You can't' follow yourself")
         else:
-            serializer.save(friendzoned_by=self.request.user,user=user_instance)
+            already_followed=Friends.objects.filter(user=user_instance,friendzoned_by=self.request.user).exists()
+            if already_followed:
+                Friends.objects.filter(user=user_instance,friendzoned_by=self.request.user).delete()
+                messages.info(self.request,"Unfollowed succesfully")
+
+            else:
+                serializer.save(friendzoned_by=self.request.user,user=user_instance)
+                messages.info(self.request,"Followed succesfully")
+
        
         
