35760578af2676a5e6cca074c0d54f331e249bf1
Hammadulhaq123 <professorcoding123@gmail.com>
2023-10-21 20:08:28 +0500

    Remove: excessive line removed

--- socialmediabackend/likes/models.py
+++ socialmediabackend/likes/models.py
@@ -6,7 +6,6 @@ from posts.models import Posts
 class Likes(models.Model):
     post = models.ForeignKey(Posts, on_delete=models.CASCADE, related_name='likes')
     liked_by = models.ForeignKey('auth.User', on_delete=models.CASCADE, default=None, blank=True, null=True)
-    # disliked_by = models.ForeignKey('auth.User', on_delete=models.CASCADE, default=None, blank=True, null=True)
 
     def __str__(self) -> str:
         return self.post.content
