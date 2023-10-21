060b45ade406cd43f342d37fb12d96fdf2500985
Hammadulhaq123 <professorcoding123@gmail.com>
2023-10-21 16:11:13 +0500

    Fix: Typo Fixed

--- socialmediabackend/likes/serializers.py
+++ socialmediabackend/likes/serializers.py
@@ -1,7 +1,7 @@
 from .models import Likes
 from rest_framework import serializers
 
-class LikesSerailizer(serializers.ModelSerializer):
+class LikesSerializer(serializers.ModelSerializer):
     liked_by = serializers.ReadOnlyField(source='liked_by.username')
     class Meta:
         model = Likes
