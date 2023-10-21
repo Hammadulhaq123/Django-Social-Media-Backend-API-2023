76f885840ffdc335ae63231c18d12cce215ebd24
Hammadulhaq123 <professorcoding123@gmail.com>
2023-10-21 13:32:53 +0500

    Update: New url_path added for CRUD of posts

--- socialmediabackend/api/urls.py
+++ socialmediabackend/api/urls.py
@@ -1,9 +1,11 @@
 from rest_framework.routers import DefaultRouter
 from users.views import UserViewset
 from user_profile.views import UserProfileView
+from posts.views import PostsViewset
 
 
 router = DefaultRouter()
 router.register(r'users', UserViewset, basename='users')
 router.register(r'profiles', UserProfileView, basename='profiles')
+router.register(r'posts', PostsViewset, basename='posts')
 urlpatterns = router.urls
