2abaf225c4256850829d2661572a86973abdc489
Hammadulhaq123 <professorcoding123@gmail.com>
2023-10-21 15:13:54 +0500

    Create: A new viewset created to seperately get all the comments and delete or update them

--- socialmediabackend/comments/views.py
+++ socialmediabackend/comments/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework import status, viewsets, permissions
+from . models import Comments
+from . serializers import CommentSerializer
+from user_profile.permissions import IsOwnerOrReadOnly
 
 # Create your views here.
+
+
+class CommentsViewset(viewsets.ModelViewSet):
+    queryset = Comments.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
