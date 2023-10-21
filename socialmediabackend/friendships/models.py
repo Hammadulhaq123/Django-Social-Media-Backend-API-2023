68ab3999468ba9d65704534484097ad6ecd12976
Hammadulhaq123 <professorcoding123@gmail.com>
2023-10-21 20:38:33 +0500

    Create: Model created that is handling the user's followers

--- socialmediabackend/friendships/models.py
+++ socialmediabackend/friendships/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from posts.models import Posts
 
 # Create your models here.
+class Friends(models.Model):
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE, related_name='followers', null=True, blank=True)
+    friendzoned_by = models.ForeignKey('auth.User', on_delete=models.CASCADE, default=None, blank=True, null=True)
+
