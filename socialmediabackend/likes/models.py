7fc5bfd8877cd3771820f9c2a6bf5f4526d6a1b8
Hammadulhaq123 <professorcoding123@gmail.com>
2023-10-21 15:50:39 +0500

    Create: A model created that contains a post and all the users who liked the post

--- socialmediabackend/likes/models.py
+++ socialmediabackend/likes/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from posts.models import Posts
+
 
 # Create your models here.
+class Likes(models.Model):
+    post = models.ForeignKey(Posts, on_delete=models.CASCADE, related_name='likes')
+    liked_by = models.ForeignKey('auth.User', on_delete=models.CASCADE, default=None, blank=True, null=True)
+    # disliked_by = models.ForeignKey('auth.User', on_delete=models.CASCADE, default=None, blank=True, null=True)
+
+    def __str__(self) -> str:
+        return self.post.content
