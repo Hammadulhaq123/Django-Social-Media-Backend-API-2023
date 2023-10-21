597b43b4297dfa1ecd38340a17448e18afa07aa5
Hammadulhaq123 <professorcoding123@gmail.com>
2023-10-21 20:49:11 +0500

    Update: object_key updated

--- socialmediabackend/friendships/serializers.py
+++ socialmediabackend/friendships/serializers.py
@@ -2,8 +2,8 @@ from .models import Friends
 from rest_framework import serializers
 
 class FriendSerializer(serializers.ModelSerializer):
-    followers = serializers.ReadOnlyField(source='friendzoned_by.username')
+    followed_by = serializers.ReadOnlyField(source='friendzoned_by.username')
     user = serializers.ReadOnlyField(source='user.username')
     class Meta:
         model = Friends
-        fields = ['id','user','followers']
+        fields = ['id','user','followed_by']
