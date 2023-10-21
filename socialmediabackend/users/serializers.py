119453703b856afca9f88715616d3ef3bb9d9f85
Hammadulhaq123 <professorcoding123@gmail.com>
2023-10-21 17:21:47 +0500

    Added new fields first_name and last_name to the User model

--- socialmediabackend/users/serializers.py
+++ socialmediabackend/users/serializers.py
@@ -6,12 +6,14 @@ class UserSerializer(serializers.ModelSerializer):
     profile_data = ProfileSerializer(read_only=True)
     class Meta:
         model = User
-        fields = ('id', 'username', 'email', 'password', 'profile_data','is_active')
+        fields = ('id', 'username', 'first_name','last_name','email', 'password', 'profile_data','is_active')
         extra_kwargs = {'email': {'required': True,
                                   'write_only': True}, 'password': {'write_only': True}}
 
     def create(self, validated_data):
         user = User(
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name'],
             email=validated_data['email'],
             username=validated_data['username']
         )
