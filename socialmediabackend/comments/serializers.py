8e2ce7cf9acd2b90cd59bf52712ba1f21accecef
Hammadulhaq123 <professorcoding123@gmail.com>
2023-10-21 13:59:25 +0500

    Create: Serializer created to serialize the data for the view

---
+++ socialmediabackend/comments/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from .models import Comments
+
+class CommentSerializer(serializers.ModelSerializer):
+    commented_by = serializers.ReadOnlyField(source='owner.username')
+    class Meta:
+        fields = ('id', 'commented_by', 'post', 'comment', 'comment_giphy', 'comment_date')
