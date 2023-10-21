8ea75875ec7668c2238d09461a7b6b195d5db99b
Hammadulhaq123 <professorcoding123@gmail.com>
2023-10-21 15:11:42 +0500

    Update: New url updated

--- socialmediabackend/api/urls.py
+++ socialmediabackend/api/urls.py
@@ -2,10 +2,11 @@ from rest_framework.routers import DefaultRouter
 from users.views import UserViewset
 from user_profile.views import UserProfileView
 from posts.views import PostsViewset
-
+from comments.views import CommentsViewset
 
 router = DefaultRouter()
 router.register(r'users', UserViewset, basename='users')
 router.register(r'profiles', UserProfileView, basename='profiles')
 router.register(r'posts', PostsViewset, basename='posts')
+router.register(r'comments', CommentsViewset, basename='comments')
 urlpatterns = router.urls
