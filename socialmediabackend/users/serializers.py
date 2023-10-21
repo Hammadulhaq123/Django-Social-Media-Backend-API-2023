f0a602c18c24c887f2e3b0132c5e44a680bb28e6
Hammadulhaq123 <professorcoding123@gmail.com>
2023-10-21 20:44:35 +0500

    Update: Serializer's updated, passed the follower's to the user serializer to populate it on frontend

--- socialmediabackend/users/serializers.py
+++ socialmediabackend/users/serializers.py
@@ -2,13 +2,19 @@ from rest_framework import serializers
 from django.contrib.auth.models import User
 from user_profile.serializers import ProfileSerializer
 from posts.serializers import PostSerializer
+from friendships.serializers import FriendSerializer
+
+
+
 
 class UserSerializer(serializers.ModelSerializer):
     profile_data = ProfileSerializer(read_only=True)
     posts = PostSerializer(many=True, read_only=True)
+    followers = FriendSerializer(many=True, read_only=True)
+
     class Meta:
         model = User
-        fields = ('id', 'username', 'first_name','last_name','email', 'password', 'profile_data','posts','is_active')
+        fields = ('id', 'username', 'first_name','last_name','email', 'password', 'profile_data','followers','posts','is_active')
         extra_kwargs = {'email': {'required': True,
                                   'write_only': True}, 'password': {'write_only': True}}
 
