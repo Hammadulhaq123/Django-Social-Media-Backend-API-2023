59f9b2e7d96f8b0ff877778ecc1598acea2420a3
Hammadulhaq123 <professorcoding123@gmail.com>
2023-10-21 15:54:24 +0500

    Create: Handling the likes and unlike by creating a viewset if user has already liked a post his like will be removed but if he's not liked the post yet it'll get liked

--- socialmediabackend/likes/views.py
+++ socialmediabackend/likes/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from posts.models import Post
+from likes.permissions import hasSelfLikedOrReadOnly
+from django.shortcuts import get_object_or_404, render
+from rest_framework import serializers, viewsets,status,permissions
+from . models import Likes
+from . serializers import LikesSerailizer
+
 
 # Create your views here.
+class LikesViewset(viewsets.ModelViewSet):
+
+    queryset=Likes.objects.all()
+    serializer_class=LikesSerializer
+    permission_classes=[permissions.IsAuthenticatedOrReadOnly,hasSelfLikedOrReadOnly]
+
+    def perform_create(self, serializer):
+        post_instance=get_object_or_404(Post,pk=self.request.data['post'])
+
+        already_liked=Likes.objects.filter(post=post_instance,liked_by=self.request.user).exists()
+        if already_liked:
+            Likes.objects.filter(post=post_instance,liked_by=self.request.user).delete()
+        else:
+            serializer.save(liked_by=self.request.user,post=post_instance)
+       
+        
