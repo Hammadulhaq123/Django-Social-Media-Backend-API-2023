d2eb7de8541bbef1f2f93c43a8594a3b0941f7ad
Hammadulhaq123 <professorcoding123@gmail.com>
2023-10-22 16:09:25 +0500

    Update: new urls added

--- socialmediabackend/api/urls.py
+++ socialmediabackend/api/urls.py
@@ -5,6 +5,8 @@ from posts.views import PostsViewset
 from comments.views import CommentsViewset
 from likes.views import LikesViewset
 from friendships.views import FriendsViewset
+from events.views import EventsViewset
+from participants.views import ParticipantViewset
 
 router = DefaultRouter()
 router.register(r'users', UserViewset, basename='users')
@@ -13,4 +15,7 @@ router.register(r'profiles', UserProfileView, basename='profiles')
 router.register(r'posts', PostsViewset, basename='posts')
 router.register(r'comments', CommentsViewset, basename='comments')
 router.register(r'likes', LikesViewset, basename='likes')
+router.register(r'events', EventsViewset, basename='events')
+router.register(r'participants', ParticipantViewset, basename='participants')
+
 urlpatterns = router.urls
