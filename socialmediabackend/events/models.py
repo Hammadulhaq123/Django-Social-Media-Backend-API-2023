72ded0d294263515b858c91d0a8f0b809089d372
Hammadulhaq123 <professorcoding123@gmail.com>
2023-10-22 17:49:02 +0500

    Update: event model updated

--- socialmediabackend/events/models.py
+++ socialmediabackend/events/models.py
@@ -8,6 +8,8 @@ class Events(models.Model):
     created_at = models.DateField(auto_now_add=True)
     event_time = models.TimeField()
     event_date = models.DateField()
+    event_location = models.CharField(max_length=1000, default="Virtual Event")
+    event_thumbnail = models.ImageField(upload_to="event_images", blank=True)
 
     def __str__(self) -> str:
         return self.event_name
