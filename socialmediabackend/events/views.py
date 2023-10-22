e3010794c532b696bf2a3c0b0925536b2e9e7f60
Hammadulhaq123 <professorcoding123@gmail.com>
2023-10-22 16:05:02 +0500

    Remove: removed Participant viewset coz created a seperate app for it

--- socialmediabackend/events/views.py
+++ socialmediabackend/events/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from rest_framework import permissions, viewsets, status
+from user_profile.permissions import IsOwnerOrReadOnly
+from .serializers import EventSerializer
+from .models import Events
 
 # Create your views here.
+
+# Viewset for all events:
+class EventsViewset(viewsets.ModelViewSet):
+    queryset = Events.objects.all()
+    serializer_class = EventSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+
+    # user can only create a post under his owenrship and can only update or delete his posts not anyone else's
+    def perform_create(self, serializer):
+        serializer.save(event_organizer=self.request.user)
+
+
