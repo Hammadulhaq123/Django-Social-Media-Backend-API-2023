65ffde98a1787388ea8de1b6539b7c89c69d0ecc
Hammadulhaq123 <professorcoding123@gmail.com>
2023-10-22 21:03:47 +0500

    Created a route for events and event creation etc

---
+++ socialmediafrontend/app/home/events/create/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+    return (
+        <div>
+            Events Create
+        </div>
+    )
+}
+
+export default page
---
+++ socialmediafrontend/app/home/events/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+    return (
+        <div>
+            Events list
+        </div>
+    )
+}
+
+export default page
