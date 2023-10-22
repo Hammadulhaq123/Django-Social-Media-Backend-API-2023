53f1a8b8d43f996f9db19c362d684bdd906f95ad
Hammadulhaq123 <professorcoding123@gmail.com>
2023-10-22 16:04:22 +0500

    Remove: removed Participant model coz created a seperate app for it

--- socialmediabackend/events/models.py
+++ socialmediabackend/events/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Events(models.Model):
+    event_organizer = models.ForeignKey("auth.User", on_delete=models.CASCADE, related_name="events")
+    event_name = models.CharField(max_length=200)
+    event_description = models.TextField(max_length=2000)
+    created_at = models.DateField(auto_now_add=True)
+    event_time = models.TimeField()
+    event_date = models.DateField()
+
+    def __str__(self) -> str:
+        return self.event_name
+
